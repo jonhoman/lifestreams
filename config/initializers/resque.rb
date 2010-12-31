534f0ccd00c952372f657c4ab7ea0bbcdf38bf9f
Jon Homan <jon@jonhoman.com>
2010-12-30 19:04:10 -0600

    adding resque initializer

---
+++ config/initializers/resque.rb
@@ -0,0 +1,7 @@
+ENV["REDISTOGO_URL"] ||= "redis://localhost:6379/"
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+
