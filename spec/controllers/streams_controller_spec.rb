d7050edd935fffaa26a6677b2c2e260a0f30075b
Jon Homan <jon@jonhoman.com>
2011-01-05 09:24:41 -0600

    clean up spec

--- spec/controllers/streams_controller_spec.rb
+++ spec/controllers/streams_controller_spec.rb
@@ -29,7 +29,6 @@ describe StreamsController do
     end
     
     it "should create a new stream" do
-      # TODO figure out how to test creation of source
       post :create, "stream" => {"name" => "Test2" }
       assigns(:stream).should_not be_nil
       assigns(:stream).name.should == "Test2"
