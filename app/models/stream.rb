0fb7ba29db838317fe0b699fb7cfc74fc4b96cdb
Jon Homan <jon@jonhoman.com>
2011-01-15 14:56:42 -0600

    add scope for all active streams

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,7 @@
 class Stream < ActiveRecord::Base
   scope :user, lambda { |user_id| where("user_id = ?", user_id) }
+  #TODO also check for "active" stream (parse-able feed, authorized twitter account)
+  scope :all_active, Stream.where("feed_id > ?", 0).where("twitter_account_id > ?", 0)
 
   belongs_to :feed 
   belongs_to :twitter_account
