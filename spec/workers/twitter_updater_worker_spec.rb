7894570dfd680b6bc4b1d361d87217db17e5a8e3
Jon Homan <jon@jonhoman.com>
2011-01-26 22:00:19 -0600

    remove test that hit twitter

--- spec/workers/twitter_updater_worker_spec.rb
+++ spec/workers/twitter_updater_worker_spec.rb
@@ -25,39 +25,4 @@ describe TwitterUpdaterWorker do
 
     @item.reload.status_id.should == "1234"
   end
-
-  context "hit twitter" do
-    xit "updates the user's timeline" do
-      TwitterUpdaterWorker.perform(@item.id, twitter_account.id)
-
-      TwitterUpdaterWorkerHelper.user_status.should == "Blog Post: #{@item.title}"
-
-      # delete status update so subsquent tests don't fail
-      TwitterUpdaterWorkerHelper.cleanup(@item.reload.status_id)
-    end
-  end
-end
-
-class TwitterUpdaterWorkerHelper
-  class << self
-    def user_status
-      #TODO create twitter username from user
-      Twitter.user_timeline("life_streams").first.text
-    end
-
-    def cleanup(status_id)
-      Twitter.client.status_destroy(status_id)
-    end
-
-    private 
-
-    def configure() 
-      Twitter.configure do |config|
-        config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
-        config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-        config.oauth_token        = ENV['DEFAULT_OAUTH_TOKEN']
-        config.oauth_token_secret = ENV['DEFAULT_OAUTH_TOKEN_SECRET']
-      end
-    end
-  end
 end
