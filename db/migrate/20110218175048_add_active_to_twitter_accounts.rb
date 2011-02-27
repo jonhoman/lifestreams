5209ba2f3cb72b3c8047db5b46aab993dd39b2a1
Jon Homan <jon@jonhoman.com>
2011-02-26 19:27:20 -0600

    fix migrations

--- db/migrate/20110218175048_add_active_to_twitter_accounts.rb
+++ db/migrate/20110218175048_add_active_to_twitter_accounts.rb
@@ -1,7 +1,6 @@
 class AddActiveToTwitterAccounts < ActiveRecord::Migration
   def self.up
     add_column :twitter_accounts, :active, :boolean, :default => true
-    TwitterAccount.all.each { |account| account.update_attributes!(:active => true) }
   end
 
   def self.down
--- db/migrate/20110225203112_add_recipients_to_email_list.rb
+++ db/migrate/20110225203112_add_recipients_to_email_list.rb
@@ -1,9 +1,9 @@
 class AddRecipientsToEmailList < ActiveRecord::Migration
   def self.up
-    add_column :email_lists, :recipients, :text
+    add_column :email_lists, :recipients_text, :text
   end
 
   def self.down
-    remove_column :email_lists, :recipients
+    remove_column :email_lists, :recipients_text
   end
 end
