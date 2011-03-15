d3602421a82b8f294d19e96132effe9393aece1a
Jon Homan <jon@jonhoman.com>
2011-03-15 17:28:34 -0500

    display access token as well

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -6,7 +6,7 @@ class FacebookController < ApplicationController
   end
 
   def callback
-    access_token = client.web_server.get_access_token(params[:code], :redirect_uri => redirect_uri)
+    @access_token = client.web_server.get_access_token(params[:code], :redirect_uri => redirect_uri)
     @user = JSON.parse(access_token.get('/me'))
 
     render :index
