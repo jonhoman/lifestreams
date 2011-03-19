e00faeaeabc5bb826b996080bca446783bad8597
Jon Homan <jon@jonhoman.com>
2011-03-19 16:29:07 -0500

    raise on unauthorized

--- lib/facebook_client.rb
+++ lib/facebook_client.rb
@@ -1,10 +1,13 @@
 class FacebookClient
-
   class << self
     def status_update(item, access_token)
       self.init(access_token)
 
-      @access_token.post 'me/feed', :message => "New Blog Post: #{item.title} #{item.link}"
+      begin
+        @access_token.post 'me/feed', :message => "New Blog Post: #{item.title} #{item.link}"
+      rescue
+        raise FacebookUnauthorizedError.new(e.message)
+      end
     end
     
     def init(access_token)
@@ -12,5 +15,5 @@ class FacebookClient
       @access_token = OAuth2::AccessToken.new(@client, access_token)
     end
   end
-
+  class FacebookUnauthorizedError < StandardError; end
 end
