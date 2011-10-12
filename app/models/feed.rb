538d43797b271fcd8d33e7d63f8b9bf9fb8d107d
Jon Homan <jon@jonhoman.com>
2011-10-12 09:36:29 -0500

    remove reference to my directory structure

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -20,7 +20,7 @@ class Feed < ActiveRecord::Base
   
   def determine_feed_url
     # stupid testing hack
-    if url == "file:///Users/jon/dev/lifestreams/spec/data/feed.rss" || url == "file:///Users/jon/dev/lifestreams/spec/data/feed-small.rss"
+    if url == "file://#{Rails.root.to_s}/spec/data/feed.rss" || url == "file://#{Rails.root.to_s}/spec/data/feed-small.rss"
       return url 
     end
 
